c3a034a6736671c87c1506cd41c2e19f161c0df1
Jacq Chin <jacq@ohmcreativestudio.com>
2016-10-03 20:11:06 +0800

    tweaked styling added js

--- script.js
+++ script.js
@@ -1,3 +1,90 @@
-$("#startBtn").click(function () {
- console.log('in');
+$( document ).ready(function() {
+    // console.log( "ready!" );
 });
+
+var questions = [
+  {
+    question: "Snape's Patronus takes the form of what animal?",
+    choices: ["Snake", "Doe", "Serpent", "Stag"],
+    correct: 1
+  },
+  {
+    question: "Who did Harry Potter bring Yule Ball?",
+    choices: ["Cho Chang", "Ginny Weasley", "Parvati Patil", "Padma Patil"],
+    correct: 2
+  },
+  {
+    question: "What type of car did Mr. Weasley bewitch to fly?",
+    choices: ["Ford Anglia", "Triumph Twelve", "Hillman Minx", "Humber Super Snipe"],
+    correct: 0
+  },
+  {
+    question: "What is the name of Ron's pet rat?",
+    choices: ["Scuffles", "Scrabbles", "Pebbles", "Shuffles"],
+    correct: 1
+  },
+  {
+    question: "How many times was Nearly Headless Nick hit in the neck with a blunt axe?",
+    choices: ["25", "35", "45", "55"],
+    correct: 2
+  },
+  {
+    question: "Who slain Lord Voldermort's last Horcrux - Nagini?",
+    choices: ["Hagrid", "Ron Weasley", "Harry Potter", "Neville Longbottom"],
+    correct: 3
+  },
+  {
+    question: "How many times was Nearly Headless Nick hit in the neck with a blunt axe?",
+    choices: ["25", "35", "45", "55"],
+    correct: 2
+  },
+  {
+    question: "How many times was Nearly Headless Nick hit in the neck with a blunt axe?",
+    choices: ["25", "35", "45", "55"],
+    correct: 2
+  },
+  {
+    question: "How many times was Nearly Headless Nick hit in the neck with a blunt axe?",
+    choices: ["25", "35", "45", "55"],
+    correct: 2
+  },
+  {
+    question: "How many times was Nearly Headless Nick hit in the neck with a blunt axe?",
+    choices: ["25", "35", "45", "55"],
+    correct: 2
+  },
+  {
+    question: "How many times was Nearly Headless Nick hit in the neck with a blunt axe?",
+    choices: ["25", "35", "45", "55"],
+    correct: 2
+  },
+  {
+    question: "How many times was Nearly Headless Nick hit in the neck with a blunt axe?",
+    choices: ["25", "35", "45", "55"],
+    correct: 2
+  }
+];
+
+// You will need to declare the following functions:
+numberOfQuestions()
+
+// It should return an integer that is the number of questions in a game
+currentQuestion()
+
+// It should return an integer that is the zero-based index of the current question in the quiz
+correctAnswer()
+
+// It should return an integer that is the zero-based index the correct answer for the currrent question
+numberOfChoices()
+
+// It should return an integer that is the number of choices for the current question
+playTurn(choice)
+
+// It should take a single integer, which specifies which choice the current player wants to make. It should return a boolean true/false if the answer is correct.
+isGameOver()
+
+// It should return a true or false if the quiz is over.
+whoWon()
+
+// It should return 0 if the game is not yet over, else it should return either 1 or 2 depending on which player won. It should return 3 if the game is a draw.
+restart()
